e9c4e78bb7acdd74822454eb07080ccdb8ec19bd
Petru Betco <petrubetco@gmail.com>
2019-05-29 17:50:15 +0300

    update

---
+++ Core/Core2/Controllers/AuthorCollectionsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Core2.Entities;
+using Core2.Models;
+using Core2.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core2.Controllers
+{
+    [Route("/api/authorCollections")]
+    public class AuthorCollectionsController : Controller
+    {
+        private readonly ILibraryRepository _libraryRepository;
+
+        public AuthorCollectionsController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+
+        [HttpPost]
+        public IActionResult CreateAuthorCollections([FromBody] IEnumerable<AuthorForCreationDto> authors)
+        {
+            var authorsForDb = new List<Author>();
+
+            _libraryRepository.AddAuthor(Mapper.Map<List<Author>;
+
+            Mapper.Map(authors, authorsForDb);
+        }
+    }
+}
