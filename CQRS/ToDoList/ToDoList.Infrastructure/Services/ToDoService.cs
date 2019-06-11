e52dd31f38f7b2fe80ddd0f02b361dc88540c53e
Petru Betco <petrubetco@gmail.com>
2019-06-11 18:04:24 +0300

    Update

---
+++ CQRS/ToDoList/ToDoList.Infrastructure/Services/ToDoService.cs
@@ -0,0 +1,14 @@
+﻿using ToDoList.Persistence;
+
+namespace ToDoList.Infrastructure.Services
+{
+    public class ToDoService
+    {
+        private readonly ToDoDbContext _context;
+
+        public ToDoService(ToDoDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
--- CQRS/ToDoList/ToDoList/Startup.cs
+++ CQRS/ToDoList/ToDoList/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using MediatR;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -26,6 +27,7 @@ namespace ToDoList
                     b => b.MigrationsAssembly(typeof(ToDoDbContext)
                         .GetTypeInfo().Assembly.GetName().Name)));
 
+            services.AddMediatR(typeof(Startup));
             services.AddMvc();
         }
 
