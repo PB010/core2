31b4169cb8c38dee5c2b694d4230ae004bef3368
Petru Betco <petrubetco@gmail.com>
2019-06-11 17:50:37 +0300

    update

--- CQRS/ToDoList/ToDoList.Persistence/Helper/DesignTimeDbContextFactory.cs
+++ CQRS/ToDoList/ToDoList.Persistence/Helper/DesignTimeDbContextFactory.cs
@@ -2,26 +2,25 @@
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using System.IO;
-using ToDoWebApi.Entities;
 
-namespace ToDoWebApi.Persistence
+namespace ToDoList.Persistence.Helper
 {
-    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ToDoContext>
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ToDoDbContext>
     {
-        public ToDoContext CreateDbContext(string[] args)
+        public ToDoDbContext CreateDbContext(string[] args)
         {
             var configuration = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json")
                 .Build();
-            
-            var builder = new DbContextOptionsBuilder<ToDoContext>();
-            
+
+            var builder = new DbContextOptionsBuilder<ToDoDbContext>();
+
             var connectionString = configuration["ConnectionString"];
-            
+
             builder.UseSqlServer(connectionString);
-            
-            return new ToDoContext(builder.Options);
+
+            return new ToDoDbContext(builder.Options);
         }
     }
 }
---
+++ CQRS/ToDoList/ToDoList/Startup.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using ToDoList.Persistence;
+using ToDoList.Persistence.Helper;
+
+namespace ToDoList
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ToDoDbContext>(options =>
+                options.UseSqlServer(_configuration["connectionString:toDoConnectionString"],
+                    b => b.MigrationsAssembly(typeof(ToDoDbContext)
+                        .GetTypeInfo().Assembly.GetName().Name)));
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ToDoDbContext context)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                DbSeeder.Migrate(context);
+            }
+
+            app.UseMvc();
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+        }
+    }
+}
